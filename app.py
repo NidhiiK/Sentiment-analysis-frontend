68919ded77074a9220045625c67546de6259d91a
Nidhi Kumari <89666195+NidhiiK@users.noreply.github.com>
2022-12-18 20:56:02 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,44 @@
+# Library imports
+import pandas as pd
+import numpy as np
+import spacy
+from sklearn.svm import LinearSVC
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline
+import joblib
+import string
+from spacy.lang.en.stop_words import STOP_WORDS
+from flask import Flask, request, jsonify, render_template
+import nltk
+
+# Load trained Pipeline
+model = joblib.load('SentimentAnalysis_Model_Pipeline.pkl')
+
+stopwords = list(STOP_WORDS)
+
+# Create the app object
+app = Flask(__name__)
+
+
+# Define predict function
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    new_review = [str(x) for x in request.form.values()]
+#     data = pd.DataFrame(new_review)
+#     data.columns = ['new_review']
+
+    predictions = model.predict(new_review)[0]
+    if predictions == 0:
+        return render_template('index.html', prediction_text='Negative')
+    else:
+        return render_template('index.html', prediction_text='Positive')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
